788d173dd4ee85496927e108022df465186d6cda
majkii2115 <szymbaramichal@gmail.com>
2020-02-06 22:11:18 +0100

    Creating Book Page UI

---
+++ Migrations/20200206205654_AddISBNToBookModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesStudyProject.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+        }
+    }
+}
